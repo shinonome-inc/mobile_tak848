4f743aa6c4ef21174f49eeb650119e5efae286a4
TAK848 <pairs_char0j@icloud.com>
2022-01-29 16:36:34 +0900

    課題1の閏年判定を実装

--- kadai01/kadai01 .playground/Contents.swift
+++ kadai01/kadai01 .playground/Contents.swift
@@ -1,3 +1,19 @@
 import UIKit
 
-var str = "Hello, playground"
+func isLeap(year: Int) -> Bool {
+    if year % 400 == 0 {
+        return true
+    } else if year % 100 == 0 {
+        return false
+    } else if year % 4 == 0 {
+        return true
+    } else {
+        return false
+    }
+}
+
+print(isLeap(year: 2000)) // true
+print(isLeap(year: 1209)) // false
+print(isLeap(year: 1980)) // true
+print(isLeap(year: 1790)) // false
+print(isLeap(year: 1993)) // false
