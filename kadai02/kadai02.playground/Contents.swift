7b6508df8f422ef3dcf00ece683f75f7e7c71ba6
TAK848 <pairs_char0j@icloud.com>
2022-01-29 17:23:01 +0900

    課題2のじゃんけんを実装

--- kadai02/kadai02.playground/Contents.swift
+++ kadai02/kadai02.playground/Contents.swift
@@ -1,3 +1,35 @@
 import UIKit
 
-var str = "Hello, playground"
+enum Hand: Int {
+    case rock = 0
+    case scissors = 1
+    case paper = 2
+}
+
+enum Result: String {
+    case win = "win!"
+    case draw = "draw"
+    case lose = "lose"
+}
+
+func janken(you: Hand) -> Void {
+    guard let cp = Hand(rawValue: Int.random(in: 0...2)) else {
+        return
+    }
+    let result: Result
+    print(you.rawValue)
+    print(cp.rawValue)
+    if you == cp {
+        result = .draw
+    } else if (you.rawValue == cp.rawValue - 1) || (you.rawValue == cp.rawValue + 2) {
+        print("win")
+        result = .win
+    }else {
+        result = .lose
+    }
+    print("cp: \(cp). You \(result.rawValue)")
+}
+
+janken(you: .rock)
+janken(you: .scissors)
+janken(you: .paper)
