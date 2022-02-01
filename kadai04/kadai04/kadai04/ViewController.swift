d7e0e171bbacb2ee7cf927b675ce77311a438647
TAK848 <pairs_char0j@icloud.com>
2022-02-01 13:09:28 +0900

    じゃんけんアプリを作成

--- kadai04/kadai04/kadai04/ViewController.swift
+++ kadai04/kadai04/kadai04/ViewController.swift
@@ -9,12 +9,85 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    let shuffleInterval: Double = 1 / 120
+    let jankenInterval: Double = 1
+    
+    @IBOutlet weak var enemyHandImageView: UIImageView!
+    @IBOutlet weak var jankenInfoLabel: UILabel!
+    @IBOutlet var handButtons: [UIButton]!
+    
+    var timer: Timer?
+    var cpHand: Hand?
+    
+    enum Hand: Int {
+        case rock
+        case scissors
+        case paper
+        var image: UIImage? {
+            switch self {
+            case .rock: return UIImage(named: "rock-hand")
+            case .scissors: return UIImage(named: "scissors-hand")
+            case .paper: return UIImage(named: "paper-hand")
+            }
+        }
+    }
+    
+    enum Result: Int {
+        case draw
+        case lose
+        case win
+        
+        var feedback: String {
+            switch self {
+            case .draw: return "draw"
+            case .lose: return "lose"
+            case .win: return "win!"
+            }
+        }
+    }
+    struct Janken {
+        var myHand: Hand
+        var cpHand: Hand
+        var result: Result {
+            return Result(rawValue: (myHand.rawValue - cpHand.rawValue + 3) % 3)!
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        timer = Timer.scheduledTimer(timeInterval: self.shuffleInterval, target: self, selector: #selector(self.shuffleCpHand(_:)), userInfo: nil, repeats: true)
     }
-
-
+    
+    @objc func shuffleCpHand(_ sender: Timer) {
+        guard let cp = Hand(rawValue: Int.random(in: 0...2)), let cpImage = cp.image else {
+            return
+        }
+        cpHand = cp
+        enemyHandImageView.image = cpImage
+        jankenInfoLabel.text = "choose your hand"
+    }
+    
+    @IBAction func buttonTapped(_ sender: Any) {
+        for button in handButtons {
+            button.isEnabled = false
+        }
+        timer?.invalidate()
+        guard let button = sender as? UIButton,
+              let myHand = Hand(rawValue: button.tag),
+              let unwrappedCpHand = cpHand
+        else {
+            return
+        }
+        
+        let janken = Janken(myHand: myHand, cpHand: unwrappedCpHand)
+        jankenInfoLabel.text = "you \(janken.result.feedback)"
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + jankenInterval) {
+            for button in self.handButtons {
+                button.isEnabled = true
+            }
+            self.timer = Timer.scheduledTimer(timeInterval: self.shuffleInterval, target: self, selector: #selector(self.shuffleCpHand(_:)), userInfo: nil, repeats: true)
+        }
+    }
 }
 
