0bc4bf7ac8d5bc432ce4e95e4af5cd623d1c0d06
TAK848 <pairs_char0j@icloud.com>
2022-02-04 22:42:19 +0900

    kadai07のプロジェクトを作成

---
+++ kadai07/kadai07/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  kadai07
+//
+//  Created by 高橋拓也 on 2022/02/04.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
