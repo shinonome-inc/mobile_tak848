e40a1d6251e3f318c7927dd30e14ee5cb0f8dfda
TAK848 <pairs_char0j@icloud.com>
2022-02-05 17:27:33 +0900

    課題08のプルジェクトを作成

---
+++ kadai08/kadai08/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  kadai08
+//
+//  Created by 高橋拓也 on 2022/02/05.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
