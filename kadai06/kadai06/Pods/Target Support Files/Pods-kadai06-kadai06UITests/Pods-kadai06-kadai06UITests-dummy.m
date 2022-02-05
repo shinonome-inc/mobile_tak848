2d399f9450378d62d2ab4662621b520c8d87b030
TAK848 <pairs_char0j@icloud.com>
2022-02-05 16:06:56 +0900

    紛れ込んだ課題6のPodsを削除

--- kadai06/kadai06/Pods/Target Support Files/Pods-kadai06-kadai06UITests/Pods-kadai06-kadai06UITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_kadai06_kadai06UITests : NSObject
-@end
-@implementation PodsDummy_Pods_kadai06_kadai06UITests
-@end
--- kadai06/kadai06/Pods/Target Support Files/Pods-kadai06/Pods-kadai06-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_kadai06 : NSObject
-@end
-@implementation PodsDummy_Pods_kadai06
-@end
