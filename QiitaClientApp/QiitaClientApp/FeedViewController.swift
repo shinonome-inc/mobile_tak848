f244258a4c8b9c1630ce211cc1705ad06827e5ab
TAK848 <pairs_char0j@icloud.com>
2022-02-15 21:26:23 +0900

    レイアウトを作成

---
+++ QiitaClientApp/QiitaClientApp/FeedViewController.swift
@@ -0,0 +1,16 @@
+//
+//  FeedViewController.swift
+//  QiitaClientApp
+//
+//  Created by 高橋拓也 on 2022/02/15.
+//
+
+import UIKit
+
+class FeedViewController: UIViewController {
+    @IBOutlet weak var feedTableView: UITableView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
