c298bbe35e2063728f59d4cb9170eafc2ebcb998
TAK848 <pairs_char0j@icloud.com>
2022-02-22 08:32:23 +0900

    日付・LGTM数表記に対応等

--- QiitaClientApp/QiitaClientApp/ArticleList/FeedViewController.swift
+++ QiitaClientApp/QiitaClientApp/ArticleList/FeedViewController.swift
@@ -10,71 +10,147 @@ import UIKit
 
 struct QiitaArticle: Codable {
     var title: String
-    var url: String
+    var url: URL
     var user: QiitaUser
+    var createdAt: Date
+    var likesCount: Int
+    public enum CodingKeys: String, CodingKey {
+        case title
+        case url
+        case user
+        case createdAt = "created_at"
+        case likesCount = "likes_count"
+    }
+}
+
+extension KeyedDecodingContainer {
+    func decode(_: Date.Type, forKey key: Key) throws -> Date {
+        let value = try decodeIfPresent(String.self, forKey: key)!
+        return ISO8601DateFormatter().date(from: value)!
+    }
 }
 
 struct QiitaUser: Codable {
     var id: String
-    var profileImageUrl: String
+    var profileImageUrl: URL
+    public enum CodingKeys: String, CodingKey {
+        case id
+        case profileImageUrl = "profile_image_url"
+    }
+}
+
+protocol WebAPIRequest: URLRequestConvertible {
+    // APIリクエストごとに変わったり変わらなかったりする
+    var baseURL: URL { get }
+    var path: String { get }
+    var method: HTTPMethod { get }
+    var parameters: Parameters { get }
+
+    // 認証が不要な場合はnil
+    var accessToken: String? { get }
+}
+
+extension WebAPIRequest {
+    var baseURL: URL { URL(string: "https://qiita.com/api/v2")! }
+    public func asURLRequest() throws -> URLRequest {
+        let request = URLRequest(url: baseURL.appendingPathComponent(path))
+        switch method {
+        case .post, .put:
+            return try JSONEncoding.default.encode(request, with: parameters)
+        default:
+            return try URLEncoding.queryString.encode(request, with: parameters)
+        }
+    }
+}
+
+struct ArticlesGetRequest: WebAPIRequest {
+    public typealias Response = [QiitaArticle]
+    let accessToken: String?
+
+    var path: String { "/items" }
+    let method = HTTPMethod.get
+
+    let page: Int
+    let query: String?
+
+    var parameters: Parameters {
+        var parameters = ["per_page": "20", "page": String(page)]
+        if let query = query {
+            parameters.updateValue(query, forKey: "query")
+        }
+        return parameters
+    }
+
+    init(accessToken: String, page: Int, query: String? = nil) {
+        self.accessToken = accessToken
+        self.page = page
+        self.query = query
+    }
+}
+
+struct UserGetRequest: WebAPIRequest {
+    var parameters: Parameters { [:] }
+    
+    public typealias Response = QiitaUser
+    let accessToken: String?
+
+    var path: String { "/authenticated_user" }
+    let method = HTTPMethod.get
+
+    init(accessToken: String) {
+        self.accessToken = accessToken
+    }
 }
 
 class FeedViewController: UIViewController {
     var articles: [QiitaArticle]?
     var searchController: UISearchController!
-    var page = 0
+    var page = 1
     @IBOutlet weak var feedTableView: UITableView!
     let refreshControl = UIRefreshControl()
-    let headers: HTTPHeaders = [
-        "Authorization": "Bearer 93748143a1e64ab5838a865f8c60cbb755e65a60"
-    ]
     var loading = false
     var searchWord: String?
+    var articleUrl: URL?
+    
+    func fetchQiitaArticleList(reload: Bool = false) {
+        if reload {
+            page = 1
+        }
+        loading = true
+        let rrrr = ArticlesGetRequest(accessToken: "93748143a1e64ab5838a865f8c60cbb755e65a60", page: page, query: searchWord)
+        AF.request(rrrr)
+            .responseDecodable(of: ArticlesGetRequest.Response.self) { response in
+                switch response.result {
+                case let .success(articles):
+                    print(self.page)
+                    self.page += 1
+                    if reload || self.articles == nil {
+                        self.articles = articles
+                    } else {
+                        self.articles?.append(contentsOf: articles)
+                    }
+                case let .failure(error):
+                    print(error)
+                    self.articles = nil
+                }
+                self.loading = false
+                self.feedTableView.reloadData()
+            }
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        feedTableView.delegate = self
+        feedTableView.dataSource = self
+        feedTableView.register(UINib(nibName: "QiitaArticleCell", bundle: nil), forCellReuseIdentifier: QiitaArticleCell.identifier)
         feedTableView.refreshControl = refreshControl
         refreshControl.addTarget(self, action: #selector(refresh(_:)), for: .valueChanged)
         setupSearchBar()
-        let decoder = JSONDecoder()
-        decoder.keyDecodingStrategy = .convertFromSnakeCase
-        page += 1
-        loading = true
-        AF.request("https://qiita.com/api/v2/items?page=\(page)&per_page=40", headers: headers)
-            .responseDecodable(of: [QiitaArticle].self, decoder: decoder) { response in
-                switch response.result {
-                case let .success(articles):
-                    self.articles = articles
-                    self.feedTableView.delegate = self
-                    self.feedTableView.dataSource = self
-                    self.feedTableView.register(UINib(nibName: "QiitaArticleCell", bundle: nil), forCellReuseIdentifier: QiitaArticleCell.identifier)
-                    self.feedTableView.reloadData()
-                    self.loading = false
-                case let .failure(error):
-                    print(error)
-                }
-            }
+        fetchQiitaArticleList()
     }
 
     @objc func refresh(_ sender: UIRefreshControl) {
-        let decoder = JSONDecoder()
-        decoder.keyDecodingStrategy = .convertFromSnakeCase
-        page = 1
-        loading = true
-        AF.request("https://qiita.com/api/v2/items?page=\(page)&per_page=4", headers: headers)
-            .responseDecodable(of: [QiitaArticle].self, decoder: decoder) { response in
-                switch response.result {
-                case let .success(articles):
-                    self.articles = articles
-                    self.feedTableView.delegate = self
-                    self.feedTableView.dataSource = self
-                    self.feedTableView.register(UINib(nibName: "QiitaArticleCell", bundle: nil), forCellReuseIdentifier: QiitaArticleCell.identifier)
-                    self.feedTableView.reloadData()
-                    self.loading = false
-                case let .failure(error):
-                    print(error)
-                }
-            }
+        fetchQiitaArticleList(reload: true)
         refreshControl.endRefreshing()
     }
 
@@ -86,20 +162,16 @@ class FeedViewController: UIViewController {
     }
 
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        print("hello")
         if segue.identifier == Segue.toArticleViewController.rawValue {
-            print("hello")
-            guard let nextVC3 = segue.destination as? UINavigationController,
-                  let nextVC2 = nextVC3.topViewController as? ArticleViewController
-            else {
-                return
+            if let nextVC3 = segue.destination as? UINavigationController,
+               let nextVC2 = nextVC3.topViewController as? ArticleViewController,
+               let articleUrl = articleUrl
+            {
+                nextVC2.articleUrl = articleUrl
+                if let indexPathForSelectedRow = feedTableView.indexPathForSelectedRow {
+                    feedTableView.deselectRow(at: indexPathForSelectedRow, animated: true)
+                }
             }
-            print("hello")
-            nextVC2.articleUrl = URL(string: "https://qiita.com")
-//            if let nextVC = nextVC {
-//                print("hello")
-//                nextVC.articleUrl = URL(string: "https://qiita.com")
-//            }
         }
     }
 }
@@ -128,12 +200,11 @@ extension FeedViewController: UITableViewDelegate, UITableViewDataSource {
         }
         if let qiitaArticleVC = storyboard?.instantiateViewController(withIdentifier: ArticleViewController.identifier) as? ArticleViewController {
             print(articles[indexPath.row])
-            qiitaArticleVC.articleUrl = URL(string: articles[indexPath.row].url)
+            qiitaArticleVC.articleUrl = articles[indexPath.row].url
             print(articles[indexPath.row].url)
+            articleUrl = articles[indexPath.row].url
             print(qiitaArticleVC.articleUrl ?? "")
-//            sleep(2)
             performSegue(segue: .toArticleViewController, sender: nil)
-//            navigationController?.pushViewController(qiitaArticleVC, animated: true)
         }
     }
 
@@ -141,75 +212,51 @@ extension FeedViewController: UITableViewDelegate, UITableViewDataSource {
         let currentOffsetY = scrollView.contentOffset.y
         let maximumOffset = scrollView.contentSize.height - scrollView.frame.height
         let distanceToBottom = maximumOffset - currentOffsetY
-        print(distanceToBottom)
         if distanceToBottom < 600, !loading {
-            let decoder = JSONDecoder()
-            decoder.keyDecodingStrategy = .convertFromSnakeCase
-            page += 1
-            loading = true
-            AF.request("https://qiita.com/api/v2/items?page=\(page)&per_page=40", headers: headers)
-                .responseDecodable(of: [QiitaArticle].self, decoder: decoder) { response in
-                    switch response.result {
-                    case let .success(articles):
-                        self.articles?.append(contentsOf: articles)
-                        self.feedTableView.delegate = self
-                        self.feedTableView.dataSource = self
-                        self.feedTableView.register(UINib(nibName: "QiitaArticleCell", bundle: nil), forCellReuseIdentifier: QiitaArticleCell.identifier)
-                        self.feedTableView.reloadData()
-                        self.loading = false
-                    case let .failure(error):
-                        print(error)
-                    }
-                }
+            fetchQiitaArticleList()
         }
+    }
+}
+
+protocol QiitaRequest {
+    var baseURL: URL { get }
+    var path: String { get }
+}
+
+extension QiitaRequest {
+    var baseURL: URL {
+        URL(string: "https://qiita.com/api/v2")!
     }
 }
 
 extension FeedViewController {
     func setupSearchBar() {
         searchController = UISearchController(searchResultsController: nil)
-//        searchController.searchResultsUpdater = self
         searchController.obscuresBackgroundDuringPresentation = false
         searchController.searchBar.delegate = self
         navigationItem.searchController = searchController
     }
 }
 
-// extension FeedViewController: UISearchResultsUpdating {
-//    func updateSearchResults(for searchController: UISearchController) {
-////        // SearchBarに入力したテキストを使って表示データをフィルタリングする。
-////        let text = searchController.searchBar.text ?? ""
-////        if text.isEmpty {
-////            filteredTitles = titles
-////        } else {
-////            filteredTitles = titles.filter { $0.contains(text) }
-////        }
-//        print(searchController.searchBar.text ?? "")
-//        feedTableView.reloadData()
-//    }
-// }
-
 extension FeedViewController: UISearchBarDelegate {
     func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
-        let decoder = JSONDecoder()
-        decoder.keyDecodingStrategy = .convertFromSnakeCase
-        
-        page = 1
-        loading = true
-        AF.request("https://qiita.com/api/v2/items?page=\(page)&per_page=40&query=\(searchBar.text!)", headers: headers)
-            .responseDecodable(of: [QiitaArticle].self, decoder: decoder) { response in
-                switch response.result {
-                case let .success(articles):
-                    self.articles = articles
-                    self.feedTableView.delegate = self
-                    self.feedTableView.dataSource = self
-                    self.feedTableView.register(UINib(nibName: "QiitaArticleCell", bundle: nil), forCellReuseIdentifier: QiitaArticleCell.identifier)
-                    self.feedTableView.reloadData()
-                    self.loading = false
-                case let .failure(error):
-                    print(error)
-                }
-            }
+        searchWord = searchBar.text
+        fetchQiitaArticleList(reload: true)
+    }
+
+    func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
+        if searchWord != nil {
+            searchWord = nil
+            fetchQiitaArticleList(reload: true)
+        }
+    }
+}
+
+extension Date {
+    var dateString: String {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy/MM/dd"
+        return formatter.string(from: self)
     }
 }
 
@@ -223,52 +270,35 @@ class QiitaArticleCell: UITableViewCell {
     
     func configure(article: QiitaArticle) {
         articleTitleLabel.text = article.title
-        articleUserIdLabel.text = article.user.id
-        articleUserImage.loadImageAsynchronously(url: URL(string: article.user.profileImageUrl))
+        articleUserIdLabel.text = "@\(article.user.id) 投稿日: \(article.createdAt.dateString) LGTM: \(article.likesCount)"
+        articleUserImage.cacheImage(imageUrl: article.user.profileImageUrl)
     }
 }
 
 extension UIImageView {
-    func loadImageAsynchronously(url: URL?, defaultUIImage: UIImage? = nil) {
-        if url == nil {
-            image = defaultUIImage
+    static let imageCache = NSCache<AnyObject, AnyObject>()
+
+    func cacheImage(imageUrl: URL) {
+        let imageUrlString = imageUrl.absoluteString
+        if let imageFromCache = UIImageView.imageCache.object(forKey: imageUrlString as AnyObject) as? UIImage {
+            image = imageFromCache
             return
         }
-
-        DispatchQueue.global().async {
-            do {
-                let imageData: Data? = try Data(contentsOf: url!)
-                DispatchQueue.main.async {
-                    if let data = imageData {
-                        self.image = UIImage(data: data)
-                    } else {
-                        self.image = defaultUIImage
-                    }
-                }
-            } catch {
-                DispatchQueue.main.async {
-                    self.image = defaultUIImage
-                }
+        URLSession.shared.dataTask(with: imageUrl) { data, _, error in
+            if error != nil {
+                return
             }
-        }
+            DispatchQueue.main.async {
+                guard let imageToCache = UIImage(data: data!) else {
+                    return
+                }
+                self.image = imageToCache
+                UIImageView.imageCache.setObject(imageToCache, forKey: imageUrlString as AnyObject)
+            }
+        }.resume()
     }
 }
 
-// extension UINavigationBar {
-//    @IBInspectable
-//    private var titleFontx: UIFont? {
-//        get {
-//            fatalError("only set this value")
-//        }
-//        set {
-//            if let newValue = newValue {
-//                titleTextAttributes = [
-//                    .font: newValue
-//                ]
-//            }
-//        }
-//    }
-// }
 import WebKit
 class ArticleViewController: UIViewController, WKUIDelegate {
     static let identifier = "ArticleViewController"
