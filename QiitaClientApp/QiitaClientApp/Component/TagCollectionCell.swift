1fdf3a60f733b3f9429be752c23d9781a4ead012
TAK848 <pairs_char0j@icloud.com>
2022-02-27 07:18:27 +0000

    セルの作成，localizeの追加

--- QiitaClientApp/QiitaClientApp/Component/TagCollectionCell.swift
+++ QiitaClientApp/QiitaClientApp/Component/TagCollectionCell.swift
@@ -5,4 +5,21 @@
 //  Created by Takuya Takahashi on 2022/03/08.
 //
 
-import Foundation
+import UIKit
+
+class TagCollectionCell: UICollectionViewCell {
+    @IBOutlet weak var tagImage: UIImageView!
+    @IBOutlet weak var tagIdLabel: UILabel!
+    @IBOutlet weak var tagItemsCountLabel: UILabel!
+    @IBOutlet weak var tagFollowersCountLabel: UILabel!
+    
+    func configure(qiitaTag: QiitaTag) {
+        if let iconUrl = qiitaTag.iconUrl {
+            tagImage.cacheImage(imageUrl: iconUrl)
+        }
+        tagIdLabel.text = qiitaTag.id
+        tagItemsCountLabel.text = "\("articles count".localized() ?? ""): \(qiitaTag.itemsCount)"
+        tagFollowersCountLabel.text = "\("followers count".localized() ?? ""): \(qiitaTag.followersCount)"
+    }
+}
+
