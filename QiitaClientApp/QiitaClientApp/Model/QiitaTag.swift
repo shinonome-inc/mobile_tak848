948cc2d83f32e464f90a6b9e48294c31ea9dd64f
TAK848 <pairs_char0j@icloud.com>
2022-03-01 11:51:09 +0000

    レスポンスの型を追加

--- QiitaClientApp/QiitaClientApp/Model/QiitaTag.swift
+++ QiitaClientApp/QiitaClientApp/Model/QiitaTag.swift
@@ -6,3 +6,16 @@
 //
 
 import Foundation
+
+struct QiitaTag: Codable {
+    var id: String
+    var iconUrl: URL?
+    var itemsCount: Int
+    var followersCount: Int
+    public enum CodingKeys: String, CodingKey {
+        case id
+        case iconUrl = "icon_url"
+        case itemsCount = "items_count"
+        case followersCount = "followers_count"
+    }
+}
