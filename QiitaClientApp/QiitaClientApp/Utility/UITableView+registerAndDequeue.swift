44eea12944ee9f591750b09aa66a8c0c3742ca8c
TAK848 <pairs_char0j@icloud.com>
2022-03-08 07:36:47 +0000

    CollectionCellのregister簡略化

--- QiitaClientApp/QiitaClientApp/Utility/UITableView+registerAndDequeue.swift
+++ QiitaClientApp/QiitaClientApp/Utility/UITableView+registerAndDequeue.swift
@@ -19,3 +19,19 @@ extension UITableView {
         dequeueReusableCell(withIdentifier: T.identifier) as? T
     }
 }
+
+extension UICollectionView {
+    func registerCustomCell<T: UICollectionViewCell>(_ cellType: T.Type) {
+        register(
+            UINib(nibName: T.identifier, bundle: nil),
+            forCellWithReuseIdentifier: T.identifier
+        )
+    }
+
+    func dequeueReusableCustomCell<T: UICollectionViewCell>(with cellType: T.Type,
+                                                            indexPath: IndexPath) -> T?
+    {
+        dequeueReusableCell(withReuseIdentifier: T.identifier,
+                            for: indexPath) as? T
+    }
+}
