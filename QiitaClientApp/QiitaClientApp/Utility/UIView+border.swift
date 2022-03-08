537fe3adb7139494e23965900bcea521547f21f9
TAK848 <pairs_char0j@icloud.com>
2022-03-08 06:38:24 +0000

    borderをstoryboardから設定できるように

--- QiitaClientApp/QiitaClientApp/Utility/UIView+border.swift
+++ QiitaClientApp/QiitaClientApp/Utility/UIView+border.swift
@@ -5,4 +5,37 @@
 //  Created by Takuya Takahashi on 2022/03/10.
 //
 
-import Foundation
+import UIKit
+
+extension UIView {
+    // 枠線の色
+    @IBInspectable var borderColor: UIColor? {
+        get {
+            layer.borderColor.map { UIColor(cgColor: $0) }
+        }
+        set {
+            layer.borderColor = newValue?.cgColor
+        }
+    }
+
+    // 枠線のWidth
+    @IBInspectable var borderWidth: CGFloat {
+        get {
+            layer.borderWidth
+        }
+        set {
+            layer.borderWidth = newValue
+        }
+    }
+
+    // 角丸設定
+    @IBInspectable var cornerRadius: CGFloat {
+        get {
+            layer.cornerRadius
+        }
+        set {
+            layer.cornerRadius = newValue
+            layer.masksToBounds = newValue > 0
+        }
+    }
+}
