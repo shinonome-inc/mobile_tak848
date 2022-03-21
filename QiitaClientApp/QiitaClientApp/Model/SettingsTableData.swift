dd6b4ea17f3a265e65b9df88eb91d42df962475f
TAK848 <pairs_char0j@icloud.com>
2022-03-21 22:24:44 +0000

    型とモデル（enum）を作成

---
+++ QiitaClientApp/QiitaClientApp/Model/SettingsTableData.swift
@@ -0,0 +1,51 @@
+//
+//  SettingsTableData.swift
+//  QiitaClientApp
+//
+//  Created by Takuya Takahashi on 2022/03/21.
+//
+
+import Foundation
+
+enum SettingsTableSection: Int, CaseIterable {
+    case appInformation
+    case others
+    var cellCount: Int {
+        switch self {
+        case .appInformation:
+            return SettingsAppInfoCell.allCases.count
+        case .others:
+            return SettingsOthersCell.allCases.count
+        }
+    }
+}
+
+enum SettingsAppInfoCell: Int, CaseIterable {
+    case privacyPolicy
+    case termsOfService
+    case appVersion
+    var titleString: String {
+        switch self {
+        case .privacyPolicy:
+            return "privacy policy".localized()!
+        case .termsOfService:
+            return "terms of service".localized()!
+        case .appVersion:
+            return "app version".localized()!
+        }
+    }
+}
+
+enum SettingsOthersCell: Int, CaseIterable {
+    case logoutOrLogin
+    var titleString: String {
+        switch self {
+        case .logoutOrLogin:
+            if QiitaAccessToken().isExist {
+                return "logout".localized()!
+            } else {
+                return "login".localized()!
+            }
+        }
+    }
+}
